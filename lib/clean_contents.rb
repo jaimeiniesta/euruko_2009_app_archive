e7597e0a3fc020aaf0a600113d53544f7d3e4a22
Jaime Iniesta <jaimeiniesta@gmail.com>
2010-08-10 15:41:56 +0200

    unify links, all go to http://euruko2009.org, forgetting app.euruko2009.org

--- lib/clean_contents.rb
+++ lib/clean_contents.rb
@@ -1,13 +1,17 @@
 # encoding: utf-8
 
 module Nanoc3::Filters
+  
   class CleanContents < Nanoc3::Filter
     identifier :clean_contents
     type :text
 
     # Some pre-cleaning on the original contents
     def run(content, params={})
-      content.gsub("div id=\"logo\"", "div id='hiddenlogo' style='display:none;'").gsub("http://euruko2009.org/archives", "/archives")
+      content.gsub!("div id=\"logo\"", "div id='hiddenlogo' style='display:none;'")
+      content.gsub!("http://euruko2009.org/archives", "/archives")
+      content.gsub!("http://app.euruko2009.org", "http://euruko2009.org")
+      content
     end
 
   end
